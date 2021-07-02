36dde114ba06c32e781528b977f20edfcdfda6f5
Gbenga Olanipekun <olabola2001@gmail.com>
2021-07-02 13:08:51 +0100

    main

---
+++ README.md
@@ -0,0 +1,12 @@
+##### This is my Calculator Website
+* There are two sections,
+1.The business logic section.
+2.The user interface section.
+3 So there is a seperation of concern
+* There images were displayed in a column
+* Css cascading were used where applicable
+* div classes were used
+* Various folders were created css,images,javascript and the html file.
+* Bootstrap was equally used.
+* there are various functions buttons which are addition,subcraction,multiplication,division ad remainder
+###### It is possible to improve on this website project in the future
---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+
+// business logic starts
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+function remainder(number1, number2) {
+  return number1 % number2;
+}
+// business logic codes end
+
+
+// user interface codes starts
+// Calculator addition starts
+//const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+//const result = add(number1, number2);
+//alert(result);
+// Calculator addition ends
+// calculator subraction starts 
+//const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+//const result = subtract(number1, number2);
+//alert(result);
+// calculator subctration ends
+
+// calculator multiplication starts 
+//const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+//const result = multiply(number1, number2);
+//alert(result);
+// calculator multiplication ends
+
+// calculator division starts 
+//const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+//const result = divide(number1, number2);
+//alert(result);
+// calculator division ends
+// calculator modulus starts 
+//const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+//const result = remainder(number1, number2);
+//alert(result);
+// calculator modulus ends
+
+
+
+
